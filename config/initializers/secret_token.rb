ddb6ca4870447c93e879593fea15ab80aa8e80c7
丁历祥 <dlx_swpu@qq.com>
2013-03-31 21:32:26 +0800

    zhiyifeedback demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedbackDemo::Application.config.secret_token = '3aaad1f690b8d0ef32ac09f41b0131510ec2878135eb82f02a243c2a890b2e5dcff7f8fd87d7fc12608ea6688df752eda086ba7a5262661b4faa68e0274dcb84'
